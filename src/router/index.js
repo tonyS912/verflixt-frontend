b1b2666dcf71143a5781efc4a42e4ba19986c2dd
Tonys <info@tony-schiller.com>
2023-11-04 08:50:09 +0100

    nearly finished

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ import ForgotPassword from "@/views/ForgotPassword.vue";
 import SuccessfullyRegistered from "@/views/SuccessfullyRegistered.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(),
     routes: [
         {
             path: '/',
